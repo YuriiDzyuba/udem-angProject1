cde913ac4ffec7fc17aa5da092ac15ca31eeb0fa
YuriiDzyuba <iurii11022@gmail.com>
2022-06-14 18:32:26 +0300

    Add rest components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './resepies/recipie-list/recipie-list.component';
+import { RecipieItemComponent } from './recipies/recipie-item/recipie-item.component';
+import { ChoppingListComponent } from './chopping-list/chopping-list.component';
+import { ShoppingEditComponent } from './chopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipiesComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    ChoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/chopping-list/chopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chopping-list',
+  templateUrl: './chopping-list.component.html',
+  styleUrls: ['./chopping-list.component.scss']
+})
+export class ChoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
